02ba52ee6e9112442d991bcc8ac02a7bf19d8897
Julien Dargelos <julien.dargelos@me.com>
2019-02-09 18:16:50 +0100

    Remove useless code

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ exports.handlers = {
   processingComplete: ({doclets}) => doclets.forEach(doclet => {
     if(!doclet.memberof) return
 
-    if(doclet.signature && doclet.signature.includes('{this}')) {
-      doclet.signature = doclet.signature.replace(/\{this\}/g, `{${linkto(doclet.memberof)}}`)
-    }
-
     ;[doclet.returns, doclet.params]
       .reduce((types, properties) => {
         if(properties) properties.forEach(({type}) => types.push(type))
